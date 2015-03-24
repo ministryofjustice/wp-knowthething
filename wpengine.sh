b7d6a66d6e3e1d497573430e5bb5f5a28e82b335
Toby Schrapel <toby@tobyschrapel.com>
2015-03-24 15:18:57 +0000

    Test

--- wpengine.sh
+++ wpengine.sh
@@ -2,13 +2,13 @@ environment=$1
 theme="sage"
 
 
-
+echo "Starting the build process..."
 if [ "$environment" == "staging" ]
 then
-  git checkout develop
+  git checkout develop &> /dev/null
 elif [ "$environment" == "production" ]
 then
-  git checkout master
+  git checkout master &> /dev/null
 else
   echo "Invalid environment."
   exit
@@ -16,18 +16,16 @@ fi
 
 if [[ -n $(git status -s) ]]
 then
-  echo "found!"
-else
-  echo "not found"
+  echo "Please review and commit your changes before continuing..."
+  exit
 fi
-exit
 
 exists=`git show-ref refs/heads/wpengine`
 if [ -n "$exists" ]
 then
-  git branch -D wpengine
+  git branch -D wpengine &> /dev/null
 fi
-git checkout -b wpengine
+git checkout -b wpengine &> /dev/null
 
 cp -r web/app wp-content
 rm "wp-content/themes/${theme}/.gitignore"
@@ -36,22 +34,22 @@ rm "wp-content/mu-plugins/disallow-indexing.php"
 rm "wp-content/mu-plugins/register-theme-directory.php"
 rm .gitignore
 echo "/*\n!wp-content/\nwp-content/uploads" >> .gitignore
-git ls-files | xargs git rm --cached
+git ls-files | xargs git rm --cached &> /dev/null
 
 cd wp-content/
 find . | grep .git | xargs rm -rf
 cd ../
 
-git add .
-git commit -am "Setting up WPEngine build."
+git add . &> /dev/null
+git commit -am "Setting up WPEngine build." &> /dev/null
 if [ "$environment" == "staging" ]
 then
   git push staging wpengine:master --force
-  git checkout develop
+  git checkout develop &> /dev/null
 elif [ "$environment" == "production" ]
 then
   git push production wpengine:master --force
-  git checkout master
+  git checkout master &> /dev/null
 fi
-git branch -D wpengine
-rm -rf wp-content/
+git branch -D wpengine &> /dev/null
+rm -rf wp-content/ &> /dev/null
