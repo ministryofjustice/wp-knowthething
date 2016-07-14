71f1df650b2df5fbc0ce90dfe355fbeeb5331967
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-07-14 17:09:15 +0100

    Update oAuth scope requirements, and configure the access type to 'online'.

--- web/app/themes/weekly/lib/users/oauth.php
+++ web/app/themes/weekly/lib/users/oauth.php
@@ -11,8 +11,8 @@ namespace Roots\Sage\Users\OAuth;
  * Alter the provider scope.
  * The default scope for Google requires too many permissions.
  *
- * @param $provider_scope
- * @param $provider
+ * @param string $provider_scope
+ * @param string $provider
  * @return string
  */
 function alter_provider_scope($provider_scope, $provider) {
@@ -23,3 +23,16 @@ function alter_provider_scope($provider_scope, $provider) {
   return $provider_scope;
 }
 add_filter('wsl_hook_alter_provider_scope', __NAMESPACE__ . '\\alter_provider_scope', 10, 2);
+
+/**
+ * Alter the provider configuration.
+ *
+ * @param array $config
+ * @param string $provider
+ * @return array
+ */
+function alter_provider_config($config, $provider) {
+  $config['access_type'] = 'online';
+  return $config;
+}
+add_filter('wsl_hook_alter_provider_config', __NAMESPACE__ . '\\alter_provider_config', 10, 2);
