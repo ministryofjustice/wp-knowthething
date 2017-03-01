c088fd46c8d740f35c722c54109a60f2e030d9bf
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-03-01 14:16:19 +0000

    Add S3 parameters to example env file, add support for Rewrite Media to S3 plugin to the theme, and define the default theme.

--- config/application.php
+++ config/application.php
@@ -55,6 +55,8 @@ define('NONCE_SALT', getenv('NONCE_SALT'));
 define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISABLE_WP_CRON', true);
 define('DISALLOW_FILE_EDIT', true);
+define('WP_DEFAULT_THEME', 'weekly');
+define('S3_UPLOADS_BASE_URL', getenv('S3_UPLOADS_BASE_URL'));
 
 /**
  * Bootstrap WordPress
